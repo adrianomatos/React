18cdfa8d48f972331b6a247cc2b7dc4ca3f5af1b
Adriano <adrianotaua@hotmail.com>
2023-11-15 22:23:40 -0300

    Renderização condicional

---
+++ README.md
@@ -0,0 +1,26 @@
+npm create vite
+nomeProjeto
+React
+Js
+cd meu-projeto
+  npm install   
+  npm run dev
+
+*Criação de Components (funcoes) - Importante na reutilização de código (export e import)
+
+*Props (conteúdo passado como parametro para a função chamada)
+
+*Eventos alterando Estados com Hooks (alterando html dinamicamente)
+
+*Renderização condicional
+
+
+
+
+
+
+
+
+
+
+
--- meu-projeto/src/components/MeuContador.jsx
+++ meu-projeto/src/components/MeuContador.jsx
@@ -2,17 +2,24 @@
 import { useState } from "react";
 
 export default function MeuContador() {
-  const [contador, setContador] = useState(0);
+  const [contador, setContador] = useState(0); // HOOK
 
   function aumentar() {
-    setContador(contador+1);
+    setContador(contador + 1);
+  }
+  function diminuir() {
+    setContador(contador - 1);
   }
 
   return (
     <div>
-      <h2>Meu Contador</h2>
-      <h3>{contador}</h3>
+      <h2>Meu Contador - Renderização condicional</h2>
+      <h3>Contador: {contador}</h3>
+
+      {contador >= 10 ? <h3>Valor alto!</h3> : <h3>Valor baixo!</h3>}
+
       <button onClick={aumentar}>Aumentar</button>
+      <button onClick={diminuir}>Diminuir</button>
     </div>
   );
 }
